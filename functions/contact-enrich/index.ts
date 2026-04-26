566982ad968d80694b1fd99a57b3c800ec4e6b6d
Rob Murchison <rob.murchison@intelligentbuildings.com>
2026-04-26 13:23:29 -0400

    Update Apollo search

--- functions/contact-enrich/index.ts
+++ functions/contact-enrich/index.ts
@@ -128,7 +128,7 @@ interface ApolloPersonRaw {
 async function searchApollo(companyName: string): Promise<ContactResult[]> {
   if (!APOLLO_KEY || !companyName) return [];
   try {
-    const res = await fetch("https://api.apollo.io/api/v1/mixed_people/search", {
+    const res = await fetch("https://api.apollo.io/api/v1/mixed_people/api_search", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
